538c3a1ae4d12186b32b7674bb26530eda564679
sorayaqz1 <soraya.zhang@gmail.com>
2017-02-01 22:35:24 -0600

    Created by Coding Ground Client

---
+++ InterviewPractice/Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci{
+
+     public static void main(String []args){
+        System.out.println("Fibonacci number");
+        printFibonacci(10);
+     }
+     
+    static int n1=0, n2=1, n3=0;
+    static void printFibonacci(int n) {
+	if (n > 0) {
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+		System.out.println(" " + n3);
+		printFibonacci(n-1);
+	}
+}
+}
